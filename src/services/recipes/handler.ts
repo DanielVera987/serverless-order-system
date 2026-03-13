e02e734fe43a9534c6943597a5901be3d9f08ce5
davera_palace <davera@thepalacecompany.com>
2026-03-12 22:42:34 -0600

    refactor: remove recipets old

--- src/services/recipes/handler.ts
+++
@@ -1,5 +0,0 @@
-import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-
-export async function getRecipes(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
-  return { statusCode: 200, body: JSON.stringify({ message: 'handler recipes updated' }) };
-}
