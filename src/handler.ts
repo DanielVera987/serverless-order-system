5fdb1c4c0c6f9bf1dea6721205278b905b1e819a
davera_palace <davera@thepalacecompany.com>
2026-03-12 22:00:36 -0600

    feat: create serverless compose

--- src/handler.ts
+++
@@ -1,5 +0,0 @@
-import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-
-export async function createOrder(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
-  return { statusCode: 200, body: JSON.stringify({ message: 'handler listo' }) };
-}
---
+++ src/services/orders/handler.ts
@@ -0,0 +1,5 @@
+import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+export async function getOrders(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+  return { statusCode: 200, body: JSON.stringify({ message: 'handler orders listo' }) };
+}
