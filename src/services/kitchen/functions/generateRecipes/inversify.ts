5141abc97cb5f7d1b2abe007f23b02c6f1489881
davera_palace <davera@thepalacecompany.com>
2026-03-14 11:41:16 -0600

    fix: use interface

--- src/services/kitchen/functions/generateRecipes/inversify.ts
+++ src/services/kitchen/functions/generateRecipes/inversify.ts
@@ -3,18 +3,19 @@ import types from './types';
 import { SQSController } from './controllers/sqs/SQSController';
 import { SqsHandler } from '../../../../context/shared/infrastructure/controller/ControllerBase';
 import { UseCase } from '../../../../context/shared/domain/UseCase';
-import GenerateRecipieUseCase from '../../../../context/kitchen/application/query/GenerateRecipieUseCase';
+import GenerateRecipieUseCase from '../../../../context/kitchen/application/create/GenerateRecipieUseCase';
 import { SQSMessageRequest } from '../../../../context/kitchen/domain/ports/SQSRequest';
 import { DynamoDBAdapter as DynamoDBAdapterDomain } from '../../../../context/shared/domain/database/DynamoDBAdapter';
 import { DynamoDBAdapter } from '../../../../context/shared/infrastructure/database/DynamoDBAdapter';
 import OrderRepository from '../../../../context/kitchen/infrastructure/repository/OrderRepository';
+import OrderRepositoryDomain from '../../../../context/kitchen/domain/repository/OrderRepository';
 import TypesShared from '../../../../context/shared/SharedTypes';
 
 const container = new Container();
 
 container.bind<SqsHandler>(types.SQSController).to(SQSController);
 container.bind<DynamoDBAdapterDomain>(TypesShared.DynamoDBAdapter).to(DynamoDBAdapter);
-container.bind<OrderRepository>(types.OrderRepository).to(OrderRepository);
+container.bind<OrderRepositoryDomain>(types.OrderRepository).to(OrderRepository);
 container.bind<UseCase<SQSMessageRequest, unknown>>(types.GenerateRecipieUseCase).to(GenerateRecipieUseCase);
 
 export default container;
