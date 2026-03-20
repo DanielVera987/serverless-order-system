2724fae31fffd4be8cce328c581710cb9c390c05
davera_palace <davera@thepalacecompany.com>
2026-03-19 22:22:55 -0600

    feat: add validate schema

--- src/services/warehouse/functions/getPurchases/handler.ts
+++ src/services/warehouse/functions/getPurchases/handler.ts
@@ -3,10 +3,15 @@ import types from './types';
 import container from './inversify';
 import { ApiGatewayHandler, ControllerBase } from '../../../../context/shared/infrastructure/controller/ControllerBase';
 import { Controllers } from '../../../../context/shared/infrastructure/controller/ControllerBase';
+import middy from '@middy/core';
+import { validationMiddleware } from '../../../../context/shared/infrastructure/middlewares/validationMiddleware';
+import { schema } from './controllers/api/schema';
 
 const controllers: Controllers = {
   api: container.get<ApiGatewayHandler>(types.ApiGatewayController),
 };
 
 const controller = new ControllerBase(controllers as Controllers);
-export const getPurchases = (event: unknown) => controller.execute(event);
+export const getPurchases = middy()
+  .use(validationMiddleware(schema))
+  .handler((event) => controller.execute(event));
